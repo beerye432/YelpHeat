202441b88145a1f07a4fdb9a8c718083454deb9c
brian clark <beerye432@gmail.com>
2015-12-05 18:01:45 -0800

    fine grain baby

--- reader.py
+++ reader.py
@@ -4,7 +4,11 @@ data = [];
 
 i = 0;
 
-lat_long_rate = open('latlongrate.txt', 'w');
+school_average = open('school_average.txt', 'w')
+
+lat_long_rate = open('latlongrate.txt', 'w')
+
+schools = dict()
 
 with open('data.json') as f:
 	for line in f:
@@ -13,8 +17,30 @@ with open('data.json') as f:
 
 		if j_content['type'] == 'business':
 
-			lat_long_rate.write(str(j_content['latitude'])+ ' ' +str(j_content['longitude']) + ' ' +str(j_content['stars']) + '\n')
+			if 'Restaurants' in j_content['categories']:
+				rating = float(j_content['stars'])
 
-lat_long_rate.close()
+				name = str(j_content['schools'])
+
+				if name in schools:
+					total_rate = schools[name][0]
+					total_num = schools[name][1]
+					schools[name] = (total_rate + rating, total_num + 1)
+				else:
+					schools[name] = (rating, 1)
+
+				lat_long_rate.write(str(j_content['latitude'])+ ' ' +str(j_content['longitude']) + '\n')
+
+				i = i + 1;
+
+for school in schools:
+	print(schools[school][1])
+	school_average.write(str(school) + ' ' + "{0:.3f}".format(schools[school][0]/schools[school][1]) + '\n')
+
+
+print(i)
+
+
+school_average.close()
 
 
